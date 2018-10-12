e32482e7bf25d1a320714a16e70439036d8d6591
Riley <harrisri@oregonstate.edu>
2018-10-11 19:21:56 -0700

    adjusted path

--- js/main.js
+++ js/main.js
@@ -25,9 +25,10 @@ function create ()
 {
     this.add.image(400, 300, 'background');
 
+    //build the map basics.
     var graphics = this.add.graphics();
     createGrid(graphics);
-    var path = this.add.path(0,50);
+    var path = this.add.path(0,75);
     path = createPath(graphics, path);
 
 }
@@ -38,29 +39,35 @@ function update ()
 
 function createPath(graphics,path){
     //Create the path that enemy units will travel on
-    path.lineTo(150,50);
-    path.lineTo(150,150);
-    path.lineTo(650,150);
-    path.lineTo(650,350);
-    path.lineTo(50,350);
-    path.lineTo(50,550);
-    path.lineTo(800,550);
+    path.lineTo(75,75);
+    path.lineTo(75,125);
+    path.lineTo(675,125);
+    path.lineTo(675,225);
+    path.lineTo(75,225);
+    path.lineTo(75,325);
+    path.lineTo(625,325);
+    path.lineTo(625,525);
+    path.lineTo(475,525);
+    path.lineTo(475,425);
+    path.lineTo(175,425);
+    path.lineTo(175,600);
+
 
     //change alpha value (third param) to 0 in order to hide path.
-    graphics.lineStyle(3,0x000000,0.8);
+    graphics.lineStyle(3,0x000000,0.5);
     path.draw(graphics);
 }
 
 function createGrid(graphics){
     graphics.lineStyle(1,0xffffff,0.5)
-    for (var i = 0; i < 9; i++) {
-          graphics.moveTo(i*100, 0);
-          graphics.lineTo(i*100, 600);
+    for (var i = 0; i < 16; i++) {
+          graphics.moveTo((i*100)/2, 0);
+          graphics.lineTo((i*100)/2, 600);
     }
 
-    for (var i = 0; i < 6;  i++) {
-        graphics.moveTo(0, i*100);      
-        graphics.lineTo(800, i*100);     
+    for (var i = 0; i < 12;  i++) {
+        graphics.moveTo(0, (i*100)/2);      
+        graphics.lineTo(800, (i*100)/2);     
     }
 
     graphics.strokePath();

74f24bcd9446ba3b93c7495085e55b1b4485ee74
Riley Harrison <harrisri@oregonstate.edu>
2018-10-23 16:58:01 -0700

    fixed startOnPath & update

--- src/sprites/Enemy.js
+++ src/sprites/Enemy.js
@@ -22,7 +22,7 @@ export default class Enemy extends Phaser.GameObjects.Sprite {
         this.follower.t = 0;
         
         // get x and y of the given t point            
-        path.getPoint(this.follower.t, this.follower.vec);
+        this.path.getPoint(this.follower.t, this.follower.vec);
         
         // set the x and y of our enemy to the received from the previous step
         this.setPosition(this.follower.vec.x, this.follower.vec.y);
@@ -30,10 +30,10 @@ export default class Enemy extends Phaser.GameObjects.Sprite {
 
     update(time, delta) {
         // move the t point along the path, 0 is the start and 0 is the end
-        this.follower.t += ENEMY_SPEED * delta;
+        this.follower.t += this.speed * delta;
             
         // get the new x and y coordinates in vec
-        path.getPoint(this.follower.t, this.follower.vec);
+        this.path.getPoint(this.follower.t, this.follower.vec);
         
         // update enemy x and y to the newly obtained x and y
         this.setPosition(this.follower.vec.x, this.follower.vec.y);
